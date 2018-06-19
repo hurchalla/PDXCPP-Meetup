dafac8c09762d8fe91994036f3d908c37da63db1
Jeff Hurchalla <hurchalla@yahoo.com>
2018-06-19 16:13:09 -0700

    Update invariants.h

--- invariants.h
+++ invariants.h
@@ -1,14 +1,14 @@
 
 #if defined(NDEBUG)
-#  define PBC_INVARIANTS_GUARD(METHOD_NAME) ((void)0)
+#  define PBC_INVARIANTS_GUARD(INVARIANTS_METHOD_NAME) ((void)0)
 #else
-#  define PBC_INVARIANTS_GUARD(METHOD_NAME) \
+#  define PBC_INVARIANTS_GUARD(INVARIANTS_METHOD_NAME) \
       using OuterClassPtrPbcInvariantsGuard = decltype(this); \
       class PbcClassInvariantsGuard { \
       public: \
           explicit PbcClassInvariantsGuard(OuterClassPtrPbcInvariantsGuard ptr)\
-             : pOuter(ptr) { pOuter->METHOD_NAME(); } \
-          ~PbcClassInvariantsGuard() { pOuter->METHOD_NAME(); } \
+             : pOuter(ptr) { pOuter->INVARIANTS_METHOD_NAME(); } \
+          ~PbcClassInvariantsGuard() { pOuter->INVARIANTS_METHOD_NAME(); } \
           PbcClassInvariantsGuard(const PbcClassInvariantsGuard&) = delete; \
           PbcClassInvariantsGuard& operator=(const PbcClassInvariantsGuard&) = delete; \
       private: \

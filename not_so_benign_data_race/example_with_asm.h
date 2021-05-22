df6fe07bbe2cb1ac04bba3fdfa5a9ba8d8e9ac4b
Jeffrey Hurchalla <hurchalla@yahoo.com>
2021-05-21 21:44:45 -0700

    Update example_with_asm.h

--- not_so_benign_data_race/example_with_asm.h
+++ not_so_benign_data_race/example_with_asm.h
@@ -1,10 +1,14 @@
 
 void processAudio(float* pInput, float const* pOutput, int sampleFrames)    
 {
-  // Yeah I know there's *technically* a data race, but it's benign.
-  // pNewAudioEffect_ is only read by this thread.
-  // I was extremely careful to check the race will never be a problem!
+  // Yeah I know there's *technically* a data race, but it's "benign" (haha, as-if).
+  // pNewAudioEffect_ is only read by this thread, so it's "safe" (except that there's
+  // no such thing as safety with a data race).
   
+  // here's one way it can go wrong:
+  // It would be weird but legal for a compiler to product the following asm.  The
+  // variable that seemed to be "read-only" would no longer be read-only, and the data
+  // race that was never "benign" to begin with, is now obviously dangerous.
   if (pNewAudioEffect_ != NULL) // mov  rax, QWORD PTR BigAudioEffect * pNewAudioEffect_
                                 // mov  QWORD PTR BigAudioEffect * pNewAudioEffect_, rax
                                 // cmp  rax, 0
